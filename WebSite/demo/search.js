5f9df8f0d56b1fddfadd83804505cdad559152e6
Berni Torres <berni.torres@gmail.com>
2016-06-06 21:21:48 -0700

    more headers

--- WebSite/demo/search.js
+++ WebSite/demo/search.js
@@ -17,6 +17,7 @@ function execSuggest()
             request.setRequestHeader("Content-Type", "application/json");
             request.setRequestHeader("Accept", "application/json; odata.metadata=none");
             request.setRequestHeader("Access-Control-Expose-Headers", "x-ms-azs-searchid");
+            request.setRequestHeader("Access-Control-Allow-Headers", "x-requested-with, x-ms-azs-return-searchid, x-ms-azs-searchid");
             request.setRequestHeader("x-ms-azs-return-searchid", "true");
         },
         type: "GET",
